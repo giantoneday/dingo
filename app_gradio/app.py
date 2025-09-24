0ed37b13470b7a2ee9de3efcef67074cc3ecb6a9
zhangjinlin <zhangjinlin@01.ai>
2025-09-24 15:52:25 +0800

    7860

--- app_gradio/app.py
+++ app_gradio/app.py
@@ -438,4 +438,4 @@ if __name__ == '__main__':
         )
 
     # 启动界面
-    demo.launch(server_name="0.0.0.0",server_port=7861, share=False)
+    demo.launch(server_name="0.0.0.0",server_port=7860, share=False)
